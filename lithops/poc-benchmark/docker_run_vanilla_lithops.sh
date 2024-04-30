a9fbfd01a8e20f21d7a1b8f2f99755282c869a23
mig <amiguel@scontain.com>
2024-04-30 11:28:41 +0200

    Lithops client deployment

---
+++ lithops/poc-benchmark/docker_run_vanilla_lithops.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+IMAGE=registry.scontain.com/amiguel/neardatapublic/lithops:alpine_python310-vanilla-lithops301
+
+CONTAINERNAME=lithopsclientalpinevan
+
+LITHOPSCONFIG=_etc_lithops_config--vanilla.txt
+
+KUBECONFIG=_root_kube_config.txt
+
+BENCHMARK=benchmark.py
+
+RUN=run.sh
+ENV=env.sh
+
+docker run -d --name $CONTAINERNAME -i -t --add-host host.docker.internal:host-gateway -v $PWD/$LITHOPSCONFIG:/etc/lithops/config -v $PWD/$KUBECONFIG:/root/.kube/config -v $PWD/$BENCHMARK:/python/benchmark.py -v $PWD/$ENV:/python/env.sh -v $PWD/$RUN:/python/run.sh --device /dev/sgx_enclave:/dev/sgx_enclave --device /dev/sgx_provision:/dev/sgx_provision --entrypoint bash $IMAGE
