bb0831c9986ad72355aef6a632497d46b18aafb9
mig <amiguel@scontain.com>
2025-10-06 16:49:33 +0200

    Build fixes

--- images/metaspace/build_metaspace.sh
+++ images/metaspace/build_metaspace.sh
@@ -2,7 +2,7 @@
 
 echo "..:INF: Usage: [ TARGET_IMAGE=organizationaldocker/metaspace:version ] $0 [ push ] # argument "push" will push the generated image to the \$TARGET_IMAGE"
 
-set -eux
+set -ex
 
 PUSH=0
 
@@ -13,7 +13,7 @@ test ! -d metaspace-py13 && git clone https://github.com/GEizaguirre/metaspace-p
 
 docker build --progress plain -f $DOCKERFILE -t $TARGET_IMAGE .
 
-if [[ "x$2" == "xpush" ]]; then
+if [[ "x$1" == "xpush" ]]; then
     PUSH=1
 fi
 
